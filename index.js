b8c261db183e4259a14065ffb76af23fec943641
Chandini Maredla <maredlachandini.20.it@anits.edu.in>
2024-10-07 16:55:57 +0530

    final push

--- index.js
+++ index.js
@@ -35,27 +35,12 @@ const app = express();
 
 // app.options('*', cors());
 
-// const corsOptions = {
-//   origin: 'https://full-real-estate.web.app', // Adjust this to match your frontend's origin
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   credentials: true,
-// };
 const corsOptions = {
-  origin: (origin, callback) => {
-    const allowedOrigins = ['https://full-real-estate.web.app'];
-
-    if (allowedOrigins.includes(origin) || origin?.startsWith('http://localhost')) {
-      callback(null, true); // Allow specific origins and any localhost
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: 'https://full-real-estate.web.app', // Adjust this to match your frontend's origin
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 };
-
 
 // Use CORS middleware
 app.use(cors(corsOptions));
